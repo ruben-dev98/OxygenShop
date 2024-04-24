090fb2ee6489a701fd2a47e56a07a65b150c1e93
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-04-24 10:32:19 +0200

    FIX: api for change coins on currency

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,7 +75,7 @@ function setDefaultPrice(mon) {
 }
 
 function getCoinsData() {
-    fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json')
+    fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json')
     .then((response) =>  {
         if(response.status === 200) {
             return response.json();
